0cb0aba1bcb0fa819dff3be682ffc9b6aa99e689
Akash Kumar <mykrapymail@gmail.com>
2020-02-29 16:54:37 -0500

    Elevated to function

--- unms_fetch/app.js
+++ unms_fetch/app.js
@@ -3,29 +3,29 @@ fs = require('fs');
 require('dotenv').config();
 const fetch = require('node-fetch')
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+function getData(endpoint){ //get the JSON devices file
+  fetch("https://"+process.env.ip+"/nms/api/v2.1/"+endpoint,
+  {
+    "method": "GET",
+    "headers": {
+      "x-auth-token":process.env.x_auth,
+      "accept": "application/json"
+    }
+  })
 
-fetch("https://"+process.env.ip+"/nms/api/v2.1/devices",
- {
-  "method": "GET",
-  "headers": {
-    "x-auth-token":process.env.x_auth,
-    "accept": "application/json"
-  }
-})
+  .then(response => {
+      return response.json();
+  })
 
-//.then(function(response){
-.then(response => {
-    return response.json();
-})
+  .then(json => {
+      let output = JSON.stringify(json)
+      fs.writeFile('../output/'+endpoint+'.json', output, (err) => {
+      if (err) console.log(err);
+      });
+  })
 
-//.then(function(json){
-.then(json => {
-    let output = JSON.stringify(json)
-    fs.writeFile('../output/devices.json', output, (err) => {
-    if (err) console.log(err);
-    });
-})
-
-.catch(err => {
-    console.log(err);
-});
+  .catch(err => {
+      console.log(err);
+  });
+};
+getData('devices')
