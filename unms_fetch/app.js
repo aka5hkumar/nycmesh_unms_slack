711564e87b58022da2060a4d3532b31e06fad55e
Akash Kumar <mykrapymail@gmail.com>
2020-02-29 23:04:46 -0500

    inital db push done

--- unms_fetch/app.js
+++ unms_fetch/app.js
@@ -3,6 +3,12 @@ fs = require('fs');
 require('dotenv').config();
 const fetch = require('node-fetch')
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+const { Client } = require('pg');
+const client = new Client({
+  user: 'postgres',
+  database: 'postgres',
+  password: process.env.postgres_pw
+})
 function getData(endpoint){ //get the JSON devices file
   fetch("https://"+process.env.ip+"/nms/api/v2.1/"+endpoint,
   {
@@ -31,13 +37,25 @@ function getData(endpoint){ //get the JSON devices file
 //getData('devices')
 
 function parse(){
+  client.connect()
+  const columns='INSERT INTO device_uptime(device_id,hostname,timestamp) VALUES($1, $2, $3) RETURNING *'
   var jsonFile = fs.readFileSync("../output/devices.json");
   dataFile=JSON.parse(jsonFile);
   for (var num in dataFile){
     var date = new Date(dataFile[num].overview.lastSeen);
-    console.log(date,'\t', dataFile[num].identification.id,'\t', dataFile[num].identification.hostname)
+    var values=[dataFile[num].identification.id, dataFile[num].identification.hostname, date]
+    client
+    .query(columns, values)
+    .then(res => {
+      console.log(res.rows[0])
+    })
+    .catch(e => console.error(e.stack))
+    //console.log(date,'\t', dataFile[num].identification.id,'\t', dataFile[num].identification.hostname)
   }
+  client.end()
 };
 
 
+
 parse()
+
